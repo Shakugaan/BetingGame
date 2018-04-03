8bdfaa347cc53c342e07b0d0b2ca4e861bc1c549
Unknown <Shakugaan01@gmail.com>
2018-04-03 13:55:44 +0200

    2D map
    Remade map

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -15,7 +15,7 @@ public class Movement : MonoBehaviour {
 	void Update () {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal,moveVertical,0.0f);
         rb.AddForce(movement * speed);
 
 	}
