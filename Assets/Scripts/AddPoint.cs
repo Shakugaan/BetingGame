5adbfbdb1f819878cb91dcadfd9994fbb0a926bb
Unknown <Shakugaan01@gmail.com>
2018-05-15 18:40:41 +0200

    Butons <3
    rawr

---
+++ Assets/Scripts/AddPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AddPoint : MonoBehaviour {
+    public Text myText = null;
+    
+    public int bet = 0;
+    
+    public void AddPoints()
+    {
+        if(myText.text=="0")
+        {
+            bet = 0;
+        }
+        bet++;
+        myText.text = "" + bet.ToString();
+    }
+    public void TakePoints()
+    {
+        if (myText.text == "0")
+        {
+            bet = 0;
+        }
+        if (bet > 0)
+        {
+            bet--;
+            myText.text = "" + bet.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+    public Text Score;
+    public Text Beting;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Food"))
+        {
+            other.gameObject.SetActive(false);
+            int bet = Int32.Parse(Score.text)+Int32.Parse(Beting.text);
+            Score.text = "" + bet.ToString();
+            Beting.text = "0";
+            
+        }
+    }
+}
